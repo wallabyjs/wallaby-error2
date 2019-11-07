9a4629d5a8a507d703ffb720d52da1e9d8811564
Simon McEnlly <simon.mcenlly@gmail.com>
2019-11-08 07:51:26 +1000

    Add package json and update jest config to run tests for all libs in mono root

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,6 @@
-/* #region file-header
- * File: jest.config.js
- * Project: DevRoot
- * Created Date: November 7 2019
- * Author: David Mann (I506251)
- * Last Modified: Thu Nov 07 2019
- * Modified By: David Mann (I506251)
- * Copyright (c) 2019 SAP SE or an SAP affiliate company, www.sap.com/copyright
- * ------------------------------------
- #endregion */
-
-
 module.exports = {
-  name: 'spo',
-  transform: {
-    '^.+\\.[t]sx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/http-client'
+  projects: [
+    'libs/sample1/jest.config.js',
+    'libs/sample2/jest.config.js'
+  ]
 };
